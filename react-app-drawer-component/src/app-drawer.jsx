f00dabe4defdb3a691cbabb347561ac0d06586e4
Garrett Quathamer <gquathamer@gmail.com>
2022-09-14 03:51:46 +0000

    toggling correctly, need to add transitioning and style it

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+export class Modal extends React.Component {
+  render() {
+    let visibility;
+    this.props.toggle ? visibility = 'show' : visibility = 'hide';
+    return <div className={`modal ${visibility}`}></div>;
+  }
+}
+
+export class Menu extends React.Component {
+  render() {
+    let visibility;
+    this.props.toggle ? visibility = 'show' : visibility = 'hide';
+    return (
+      <div className={`menu ${visibility}`}>
+        <h1>Menu</h1>
+        <ul>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Get Started</a>
+          </li>
+          <li>
+            <a href="">Sign In</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: true
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    if (this.state.isOpen) {
+      return (
+        <div className='container' onClick={this.handleClick}>
+          <Menu toggle={this.state.isOpen} />
+          <Modal toggle={this.state.isOpen} />
+        </div>
+      );
+    } else {
+      return (
+        <div className='container'>
+          <FontAwesomeIcon size='2xl' icon={faBars} onClick={this.handleClick}/>
+        </div>
+      );
+    }
+  }
+}
