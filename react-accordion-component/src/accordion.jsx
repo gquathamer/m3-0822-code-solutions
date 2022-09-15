c0514d7a20c1838d6f5220be1b9f2c2dcdbe47cd
Garrett Quathamer <gquathamer@gmail.com>
2022-09-15 23:25:58 +0000

    completed react-accordion-component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class TopicCard extends React.Component {
   render() {
     let visibility;
-    this.props.clickedTopicID === this.props.id ? visibility = 'show' : visibility = 'hide';
+    this.props.clickedTopicID === this.props.id && this.props.clickCount < 1 ? visibility = 'show' : visibility = 'hide';
     return (
       <div className='topic-card'>
         <h1 className='topic-header no-margin padding' id={this.props.id} onClick={this.props.onClick}>{this.props.topic}</h1>
@@ -17,14 +17,24 @@ export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clickedTopic: ''
+      clickedTopic: '',
+      clickCount: 0
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
-    this.setState({
-      clickedTopic: event.target.id
+    this.setState(previousState => {
+      let newClickCount = previousState.clickCount;
+      if (previousState.clickedTopic === event.target.id && newClickCount < 1) {
+        newClickCount++;
+      } else {
+        newClickCount = 0;
+      }
+      return {
+        clickedTopic: event.target.id,
+        clickCount: newClickCount
+      };
     });
   }
 
@@ -33,7 +43,7 @@ export default class Accordion extends React.Component {
       <div className='container'>
         {
           this.props.topics.map(element => {
-            return <TopicCard key={element.id} clickedTopicID={this.state.clickedTopic} id={element.id} topic={element.topic} detail={element.detail} onClick={this.handleClick} />;
+            return <TopicCard key={element.id} clickCount={this.state.clickCount} clickedTopicID={this.state.clickedTopic} id={element.id} topic={element.topic} detail={element.detail} onClick={this.handleClick} />;
           })
         }
       </div>
